a14533d6eb7c06f4df12d637ca91b18a086762cf
deepmiemie <123058879+deepmiemie@users.noreply.github.com>
2024-01-08 17:24:43 +0800

    11
    1

--- backproject/app01/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-class Customed_User(models.Model):
-    username=models.CharField(max_length=12)
-    password=models.CharField(max_length=20)
-
-class Media(models.Model):
-    file = models.FileField(upload_to="uploadimg/")
--- backproject/ut.py
+++
@@ -1,2 +0,0 @@
-from math import *
-print(pow(4**5*exp(4)/(2880)*2*10**3,1/4))
